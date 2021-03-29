6ff75ffb3067ce6dc8d9a6de8523ed073273026d
erol <je.evangelista04@gmail.com>
2021-03-29 17:10:37 -0400

    first commit

---
+++ schema.py
@@ -0,0 +1,62 @@
+import json
+import requests
+import time
+from glob import glob
+import os
+
+APIURL = os.getenv("URL", "https://maayanlab.cloud/sigcom-lincs/metadata-api/")
+auth = (os.getenv("USERNAME", "admin"), os.getenv("password", "admin"))
+res = requests.get(APIURL + "schemas")
+for s in res.json():
+	r = requests.delete(APIURL + "schemas" + "/"+s["id"], auth=auth)
+	if not r.ok:
+		print(s["id"])
+
+# with open("schemas.json") as o:
+# 	schemas = json.loads(o.read())
+# 	for s in schemas:
+# 		res = requests.post(APIURL + "schemas", json=s, auth=auth)
+# 		if not res.ok:
+# 			print(s["id"])
+# 			print(res.text)
+
+with open("schemas/counting.json") as o:
+	schemas = json.loads(o.read())
+	for schema in schemas:
+		res = requests.post(APIURL + "schemas", json=schema, auth=auth)
+		if not res.ok:
+			print(schema["id"])
+			print(res.text)
+
+for filename in glob('schemas/ui-schemas/*.json'):
+	with open(filename) as o:
+		schema = json.loads(o.read())
+		res = requests.post(APIURL + "schemas", json=schema, auth=auth)
+		if not res.ok:
+			print(schema["id"])
+			print(res.text)
+
+with open("schemas/landing.json") as o:
+	schema = json.loads(o.read())
+	res = requests.post(APIURL + "schemas", json=schema, auth=auth)
+	if not res.ok:
+		print(schema["id"])
+		print(res.text)
+		
+
+res = requests.get(APIURL + "optimize/refresh", auth=auth)
+
+res = requests.get(APIURL + "optimize/status", auth=auth)
+while (not res.text=="Ready"):
+	time.sleep(2)
+	res = requests.get(APIURL + "optimize/status", auth=auth)
+print(res.text)
+
+
+res = requests.get(APIURL + "summary/refresh", auth=auth)
+
+res = requests.get(APIURL + "summary/status", auth=auth)
+res = requests.get(APIURL + "summary/status", auth=auth)
+while (not res.text=="Ready"):
+	time.sleep(2)
+	res = requests.get(APIURL + "summary/status", auth=auth)
---
+++ update_libraries.py
@@ -0,0 +1,15 @@
+import json
+import requests
+import time
+import os
+
+APIURL = os.getenv("URL", "https://maayanlab.cloud/sigcom-lincs/metadata-api/")
+auth = (os.getenv("USERNAME", "admin"), os.getenv("password", "admin"))
+
+with open("libraries.json") as o:
+	libraries = json.loads(o.read())
+
+for lib in libraries:
+	res = requests.patch(APIURL+"/libraries/"+lib["id"], json=lib, auth=auth)
+	if not res.ok:
+		print(res.text)
