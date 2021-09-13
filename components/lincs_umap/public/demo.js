8d293d3ddf7afdc3bcc2eb47f3f356fd5bc86054
erol <je.evangelista04@gmail.com>
2021-09-13 18:56:47 -0400

    deleted components

--- components/lincs_umap/public/demo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LINCSUMAP from '../src/index'
-
-export default class Demo extends React.Component {
-  render = () => (
-    <LINCSUMAP/>
-  )
-}
--- components/lincs_umap/public/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Demo from './demo.js'
-
-const app = document.getElementById("app");
-
-ReactDOM.render(<Demo />, app)
